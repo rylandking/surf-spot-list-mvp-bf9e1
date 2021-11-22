c500f3b1faea516da245cd039ad04cefd8c9a1b0
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 20:46:34 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export default function SurfSpotSection({ annotationPrefix, title }) {
     return (
         <div data-sb-field-path={annotationPrefix}>
-            Airport Rights' surf is an intermediate right reef break.
+            <p>Airport Rights' surf is an intermediate right reef break.</p>
         </div >
     )
 }
