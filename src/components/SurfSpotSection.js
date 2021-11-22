9130e87ac37970c33b89416338b5f13073bcfa3a
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 20:46:29 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -4,12 +4,6 @@ export default function SurfSpotSection({ annotationPrefix, title }) {
     return (
         <div data-sb-field-path={annotationPrefix}>
             Airport Rights' surf is an intermediate right reef break.
-
-The waves at Airport Rights are at their best with a 13 to 16 second Southwest swell at around 3 to 5 feet. If you get it with a light East wind at low tide, it’ll be close to as good as it gets — it’s a 4 star surf spot, so that means the surf would be insanely fun for intermediate surfers.
-
-The best time to surf Airport Rights is in May, June, July or August.
-
-Watch this video of Airport Rights' surf to get a better feel for what the waves are like.
         </div >
     )
 }
