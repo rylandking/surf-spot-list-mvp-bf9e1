895e4c66dd97d2aeb89cddfb81531f65657f4e60
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 20:57:19 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default function SurfSpotSection({ annotationPrefix, title }) {
+export default function SurfSpotSection({ annotationPrefix, titleContraction }) {
     return (
         <div data-sb-field-path={annotationPrefix}>
             <p data-sb-field-path={titleContraction}>{titleContraction} surf is an intermediate right reef break.</p>
