cbdf83b7198e0a12476a0301889b2da530ea80b2
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 20:56:12 +0000

    PostLayout.js: changed

--- src/components/PostLayout.js
+++ src/components/PostLayout.js
@@ -19,7 +19,7 @@ export default function PostLayout(props) {
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
-    let titleContraction;
+    const titleContraction;
     // Set surf spot contraction
     if (title.endsWith('s')) {
         titleContraction = `${title}'`;
