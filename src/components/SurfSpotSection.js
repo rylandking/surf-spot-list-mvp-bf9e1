5307a7af0cf3e3ee78a7be82a01eb13f9972a032
Stackbit Code Editor <projects@stackbit.com>
2021-11-22 20:26:51 +0000

    SurfSpotSection.js: changed

--- src/components/SurfSpotSection.js
+++ src/components/SurfSpotSection.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default function SurfSpotSection({ body }) {
+export default function SurfSpotSection({ annotationPrefix, body }) {
     return (
         <div data-sb-field-path={annotationPrefix}>
             <p data-sb-field-path=".body">{body}</p>
